0f9479092792d94ec44f97c079330acfc1e8d33f
Jose Roberto Sanchez <111701453+Cavoh@users.noreply.github.com>
2025-11-03 12:52:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function verificarSalario() {
   var salario = parseFloat(document.getElementById("salario").value);
   var auxTrans = document.getElementById("auxTrans");
-  if (salario < 2320000) {
+  if (salario < 2847000) {
     auxTrans.style.display = "block";
   } else {
     auxTrans.style.display = "none";
@@ -116,3 +116,4 @@ totalElement.value = "$" + sumarValores().toLocaleString("es-ES", { maximumFract
 totalElement.style.color = "red";
 
 }
+
